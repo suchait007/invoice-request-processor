00e33536b124008f1579be62cc08c98df5a0264c
Suchait Gaurav <>
2024-02-12 13:47:17 +0530

    updating all files.

--- src/main/java/com/invoice/request/processor/function/repos/AccessLogRepo.java
+++
@@ -1,7 +0,0 @@
-package com.invoice.request.processor.function.repos;
-
-import com.invoice.request.processor.function.entities.AccessLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccessLogRepo extends JpaRepository<AccessLog, String> {
-}
